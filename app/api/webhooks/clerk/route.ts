8c1ac3a3730620931c36402329b9131635342b4b
Justine Edward Santos <hydxenonmain@gmail.com>
2024-06-15 16:18:28 +0800

    Added route.ts

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,11 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook } from 'svix'
+
+import { createUser } from "@/lib/actions/user.action";
+
+export async function POST(req: Request){
+    
+}
