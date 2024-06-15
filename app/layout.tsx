84b812d0a9ef8403b037dd8481d02fe112225944
Justine Edward Santos <hydxenonmain@gmail.com>
2024-06-15 14:38:38 +0800

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} min-h-screen bg-background antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/public_form/page.tsx
@@ -0,0 +1,5 @@
+const PublicForm = () => {
+  return <div>PublicForm</div>;
+};
+
+export default PublicForm;
