9c2e41de20ab13c72591f8ba76b603996dc29b51
Justine Edward Santos <hydxenonmain@gmail.com>
2024-06-16 22:04:33 +0800

    default route to survey-form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   ClerkProvider,
   SignInButton,
@@ -6,11 +7,20 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/survey-form");
+  }, []);
+
   return (
     <ClerkProvider>
       <html lang="en">
